a2bb0705151ecf26015f7e2e994c65a795c95764
Marcelo Correia <marcelo@hey.ho>
2018-01-06 10:42:27 +1100

    Updating taps

--- go-template-engine.rb
+++ go-template-engine.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class GoTemplateEngine < Formula
   homepage 'https://github.com/marcelocorreia/go-template-engine'
-  url 'https://github.com/marcelocorreia/go-template-engine/releases/download/1.60.0/go-template-engine-darwin-amd64-1.62.0.zip'
+  url 'https://github.com/marcelocorreia/go-template-engine/releases/download/2.0.0/go-template-engine-darwin-amd64-2.0.0.zip'
   version '1.62.0'
   sha256 '026df6edd153798ef0a1e6d649a13048f5feec10285df2df42288be3c91363ad'
 
