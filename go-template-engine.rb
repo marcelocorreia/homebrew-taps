a86795eb2f6d6aee9a4ffc42cd5b272e659e7655
Marcelo Correia <marcelo@correia.io>
2019-04-15 23:01:53 +1000

    Release go-template-engine 3.1.3

--- go-template-engine.rb
+++ go-template-engine.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class GoTemplateEngine < Formula
   homepage 'https://github.com/marcelocorreia/go-template-engine'
-  url 'https://github.com/marcelocorreia/go-template-engine/releases/download/3.0.8/go-template-engine-darwin-amd64-3.0.8.zip'
-  version '3.0.8'
-  sha256 'd4aec283769afca5ef2d79a9082de2231e7dc865cd0140d9f78d85ae72280e5b'
+  url 'https://github.com/marcelocorreia/go-template-engine/releases/download/3.1.3/go-template-engine-darwin-amd64-3.1.3.zip'
+  version '3.1.3'
+  sha256 '0b445a69179bb9741462c2350629b6c373f2c0faaa2efbbb930894b889c8adf4'
 
   depends_on :arch => :x86_64
 
