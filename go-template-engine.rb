3421a0b409a9abe113db7f81e0420f15d5fa9242
marcelocorreia <marcelo@correia.io>
2019-05-09 14:44:17 +1000

    Brew formula update for go-template-engine version 3.3.2

--- go-template-engine.rb
+++ go-template-engine.rb
@@ -2,9 +2,9 @@
 class GoTemplateEngine < Formula
   desc "Go Template Engine (GTE) - Easy util to apply Go Templates in BAU jobs"
   homepage "https://marcelo.correia.io"
-  url "https://github.com/marcelocorreia/3.3.0/go-template-engine-3.3.0-darwin-x86_64.tar.gz"
-  version "3.3.0"
-  sha256 "0fe8dbec2e6edc1b4b9f7701a382e17e2c8bd08b12df2834617e5cc5ffd95335"
+  url "https://github.com/marcelocorreia/go-template-engine/releases/download/3.3.2/go-template-engine-3.3.2-darwin-x86_64.tar.gz"
+  version "3.3.2"
+  sha256 "151aa857a49103b979db5d4fc616933367b24ec69ec7993747416554c3d72ce3"
 
   def install
     bin.install "go-template-engine"
